107fd9c79e2c35a14fb815f446ed01975737727c
Clement <clbernar@Inception.local>
2024-03-22 20:39:59 +0100

    Finishing

--- srcs/requirements/mariadb/tools/create_database.sh
+++ srcs/requirements/mariadb/tools/create_database.sh
@@ -1,38 +1,23 @@
 #Start le service mariadb
 service mariadb start;
 
-#Les variables d'env doivent etre ajoutees au .env
-#echo " $SQL_DATABASE    C'est un test";
 #Creation d'une table si elle n'existe pas du nom de ${SQL_DATABASE}
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-#mysql -e "CREATE DATABASE IF NOT EXISTS TestDB;" #TEST V
 
 #Creation d'un utilisateur s'i n'existe pas deja avec son mot de passe associe
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-#mysql -e "CREATE USER IF NOT EXISTS CLB@'localhost' IDENTIFIED BY 'IncCLBPassWord24';"
-#mysql -e "Show Databases;"
 
-#Je donne les tout les droits a l'utilisateur que je viens de creer sur la DB
-#mysql -e " GRANT ALL PRIVILEGES ON TestDB.* TO CLB@'%' IDENTIFIED BY 'IncCLBPassWord24';"
-#mysql -e " GRANT ALL PRIVILEGES ON TestDB.* TO CLB@'localhost' IDENTIFIED BY 'IncCLBPassWord24';"
+#Je donne tout les droits sur la DB a l'utisateur que je viens de creer 
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+#mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 
 #Je change le mot de passe de l'utilisateur root
-#mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'IncPassWord24';"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 
 #Je rafraichis pour que mysql prenne tout en compte
-#mysql -e "SHOW GRANTS FOR 'root'@'localhost';"
-#L'erreur est que j'ai set un mot de passe a root et que je dois me connecteren tant que root pour faire FLUSH PRIVILEGES
-#mysql -u root -p'IncPassWord24' -e "FLUSH PRIVILEGES;"
 mysql -u root -p"$SQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
-#mysql -e "FLUSH PRIVILEGES;"
 
 #Redemarrage de mysql
-#mysqladmin -p'IncPassWord24' shutdown
 mysqladmin -p"${SQL_ROOT_PASSWORD}" shutdown
-#mysqladmin -u root -p'IncPassWord24' shutdown
-#mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdow
 exec mysqld_safe
 
--- srcs/requirements/wordpress/tools/auto-config.sh
+++ srcs/requirements/wordpress/tools/auto-config.sh
@@ -1,39 +1,32 @@
 #Pour etre sur que la DB MariaDB a eu le temps de se lancer
-sleep 10;
-
-echo "User is : $(whoami)";
+sleep 8;
 
 if [ ! -f "/var/www/wordpress/wp-config.php" ]; then
 	cd /var/www/wordpress/
-	echo "Ca rentre dans le if de mon script = FileCreation $SQL_DATABASE $SQL_USER
-	
-$SQL_PASSWORD " && 
 	wp config create --allow-root \
 	--dbname=$SQL_DATABASE \
 	--dbuser=$SQL_USER \
 	--dbpass=$SQL_PASSWORD \
 	--dbhost=mariadb:3306 \
-	#--path='/var/www/wordpress';
+	--path='/var/www/wordpress';
 fi
 
-echo "TEST1";
 
-wp core install
+
+wp core install --allow-root \
 	--url=$DOMAIN_NAME \
-	--allow-root \
-     	#--path='/var/www/wordpress' \
-	--title=$WP_TITLE \
+	--title="$WP_TITLE" \
 	--admin_user=$ADMIN_WP_USER \
 	--admin_password=$AMIN_WP_PASSWORD \
 	--admin_email=$ADMIN_WP_EMAIL;
-
-echo "TEST2";
+#	--path='/var/www/wordpress';
+	
 wp user create --allow-root --path='/var/www/wordpress'  bob bob@example.com --user_pass=$EX_WP_PASSWORD
-# ?
+
 if [ ! -d "/run/php" ] ; then
-	echo "Ca rentre dans le if /run/php n'existe pas" && mkdir /run/php;
+	mkdir /run/php;
 fi
 
-echo "Test3";
+#chown -R www-data:www-data /var/www/wordpress
 # Lancer php-fpm
 /usr/sbin/php-fpm7.4 -F
